85e7625a0300b489eb13caac3118c29886e40427
Naresh Samal <nareshsamal99384@gmail.com>
2026-08-29 15:49:14 +0530

    build(dist): compile fresh production assets with admin user fixes

--- build/server.js
+++ build/server.js
@@ -266,6 +266,7 @@ async function startServer() {
       if (error)
         throw error;
       const mapped = users.map((au) => ({
+        id: au.id,
         uid: au.id,
         email: au.email,
         displayName: au.user_metadata?.displayName || au.user_metadata?.full_name || au.user_metadata?.name || au.email?.split("@")[0],
